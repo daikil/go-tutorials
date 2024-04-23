2c53285b52c824b574616eeb6c9c27e007ec7f47
daiki oikawa <daiki404@icloud.com>
2024-04-23 15:58:04 +0900

    Create a module 完了

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
-	fmt.Println(quote.Glass())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
+	// Request a greeting message.
+	// message, err := greetings.Hello("Gladys")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
+
+	// If no error was returned, print the returned message
+	// to the console.
+	// fmt.Println(message)
 }
